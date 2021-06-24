3b033a54eb0a0da9ce58700b0ce7f217a04a310c
Darquedante <lilchemist@gmail.com>
2021-06-24 22:04:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,103 @@
+"""
+This is a Project by G-Man.
+The Project name is " Pokemon! "
+"""
+print(" ____    ___           ___             ___            ")
+print("|    |  /   \  |   /  |     |\    /|  /   \  |\  |  | ")  
+print("|____|  |-0-|  |---   |---  | \  / |  |-0-|  | \ |  | ")
+print("|       \___/  |   \  |___  |  \/  |  \___/  |  \|  0 ")
+
+name = input("What is your name?: ")
+
+pokemon = int(input(name+"!" + " Pick a number for a Pokemon (1-4): "))
+
+pokeball = pokemon
+
+pokemon = pokeball % 1
+
+if pokeball == 1:
+ print("CONGRADULATIONS! " + name +" got a Pikachu!")
+
+ print("      !PIKACHU!           ")
+ print("       ^     ^            ")
+ print("      | |   | |           ")
+ print("      | |___| |           ")
+ print("     /         \_______   ")
+ print("    |  |  .  | |       |  ")
+ print("    |o    W   o|  _____|  ")
+ print("    \          / /        ")
+ print("     |U      U|\ \        ")
+ print("     | ______ |/_/        ")
+ print("     U       U            ")
+
+if pokeball == 2:
+ print("CONGRADULATIONS! " + name +" got a Charmander!")
+    
+ print("      !CHARMANDER!        ")    
+ print("        _____             ")
+ print("       /     \    /\      ")
+ print("       ||  | |  <   >     ")
+ print("     ^^\ u   /^^  \/      ")
+ print("      \\|   |//__/ /      ")
+ print("        | _ |_____/       ")
+ print("       /_/ \_\            ")
+ 
+if pokeball == 3:
+ print("CONGRADULATIONS! " + name +" got a Bulbasaur!")
+     
+ print("       !BULBASAUR!        ")
+ print("          ____            ")
+ print("         /    \           ")
+ print("   /\__/\|____|           ")
+ print("   ||  |       \          ")
+ print("   \_W__/  //  |          ")
+ print("      ( (  )(  /          ")
+ 
+if pokeball == 4:
+ print("CONGRADULATIONS! " + name +" got a Squirtle!")
+    
+ print("        !SQUIRTLE!        ")
+ print("          _____           ")
+ print("         /     \   ___    ")
+ print("         ||,,| |  /   \   ")
+ print("         \ _W_ /  | \_/   ")
+ print("         /|\|/|\_/ /      ")
+ print("         w\/|\/w__/       ")
+ print("           w w            ")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
